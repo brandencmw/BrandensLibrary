40a7dc780d3bc7acd21730f1ad9f20ad9a8a1935
Branden Wheeler <branden.wheeler17@gmail.com>
2021-12-23 11:53:20 -0500

    Separate author app from book app

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Author(models.Model):
+    id = models.UUIDField(default=uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    headshot = models.ImageField(null=True, blank=True, default="default.jpg")
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
--- books/models.py
+++ books/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 import uuid
+from authors.models import Author
 
 
 
-class Author(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    created = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    headshot = models.ImageField(null=True, blank=True, default="default.jpg")
 
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+# class Author(models.Model):
+#     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+#     created = models.DateTimeField(auto_now_add=True)
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     headshot = models.ImageField(null=True, blank=True, default="default.jpg")
+
+#     def __str__(self):
+#         return self.first_name + " " + self.last_name
 
 class Book(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
