7782c7384f99bd1a99732a7a226cf3a905d9ecf9
Branden Wheeler <branden.wheeler17@gmail.com>
2021-12-23 15:02:11 -0500

    Add tag and book data

--- books/models.py
+++ books/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 from uuid import uuid4
 from authors.models import Author
+from tags.models import Tag
 
 class Book(models.Model):
+    BOOK_CATEGORY = (
+        ("f", "Fiction"),
+        ("nf", "Non Fiction")
+    )
+
     id = models.UUIDField(default=uuid4, unique=True, primary_key=True, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     publish_year = models.IntegerField(null=True, blank=True)
+    length = models.IntegerField(null=True, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+    category = models.CharField(max_length=200, choices=BOOK_CATEGORY, null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, default="")
+    tags = models.ManyToManyField(Tag, blank=True)
     cover_img = models.ImageField(null=True, blank=True, default="default.jpg")
 
     def __str__(self):
