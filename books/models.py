2d3a3026698367db0abf2bfe010e61c5ee3a9102
Branden Wheeler <branden.wheeler17@gmail.com>
2021-12-23 11:09:11 -0500

    Add default images and footer content

--- books/models.py
+++ books/models.py
@@ -8,6 +8,7 @@ class Author(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    headshot = models.ImageField(null=True, blank=True, default="default.jpg")
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -18,6 +19,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, default="")
+    cover_img = models.ImageField(null=True, blank=True, default="default.jpg")
 
     def __str__(self):
         return self.title
