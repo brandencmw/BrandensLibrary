77292d0de414e8d8569ebb0cd7bf61a75bd79daa
Branden Wheeler <branden.wheeler17@gmail.com>
2021-12-23 11:40:33 -0500

    Create authors app

--- BrandensLibrary/settings.py
+++ BrandensLibrary/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
