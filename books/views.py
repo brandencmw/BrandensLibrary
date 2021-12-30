bf2c44188db637b083a45d287ba99107d05347f3
Branden Wheeler <branden.wheeler17@gmail.com>
2021-12-30 11:12:30 -0500

    Add recently added section

--- books/views.py
+++ books/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
+from django.db.models import Q
 from .models import Book, Tag
 from django.http import HttpResponse
 
 
 
 def home(request):
-    return render(request, "index.html")
+
+    recently_added = Book.objects.all().order_by("created")[:5:-1]
+    context = {
+        "recently_added":recently_added
+    }
+
+    return render(request, "index.html", context)
 
 def search(request):
     search_terms = request.GET["search-terms"].split(" ")
@@ -17,7 +24,6 @@ def search(request):
                 results.append(result)
 
     context = {'search_results': results}
-    print(results)
     return render(request, "search-results.html", context)
 
 def book(request, key):
@@ -27,13 +33,52 @@ def book(request, key):
     context = {'book': book, 'author':author, 'tags':tags}
     return render(request, "single-book.html", context)
 
+
+def valid_query(param):
+    return param != "" and param is not None
+
 def browse(request):
     books = Book.objects.all()
-    tags = Tag.objects.all()
+
+    author_name_query = request.GET.get('author_name')
+    len_start_query = request.GET.get('len_range_start')
+    len_end_query = request.GET.get('len_range_end')
+    pub_start_query = request.GET.get('pub_range_start')
+    pub_end_query = request.GET.get('pub_range_end')
+    fiction_query = request.GET.get('fiction_check')
+    nonfiction_query = request.GET.get('nonfiction_check')
+    tag_query = request.GET.getlist('tag_list')
+
+    if valid_query(author_name_query):
+        books = books.filter(Q(author__first_name__icontains=author_name_query) | Q(author__last_name__icontains=author_name_query))
+
+    if valid_query(len_start_query):
+        books = books.filter(length__gte=len_start_query)
+
+    if valid_query(len_end_query):
+        books = books.filter(length__lte=len_end_query)
+
+    if valid_query(pub_start_query):
+        books = books.filter(publish_year__gte=pub_start_query)
+    
+    if valid_query(pub_end_query):
+        books = books.filter(publish_year__lte=pub_end_query)
+
+    if nonfiction_query is None and fiction_query is not None:
+        books = books.filter(category="f")
+    elif fiction_query is None and nonfiction_query is not None:
+        books = books.filter(category="nf")
+
+    if tag_query != []:
+        books = books.filter(tags__name__in=tag_query)
+
     context = {
-        'books':books,
-        'tags':tags
+        'books':books.distinct(),
+        'tags':Tag.objects.all()
     }
+
     return render(request, "browse-books.html", context)
+
+
         
 
