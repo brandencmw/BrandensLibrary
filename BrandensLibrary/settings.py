f478347e174a595c6e651ef2573c04d6e3650153
Branden Wheeler <branden.wheeler17@gmail.com>
2022-01-05 15:59:35 -0500

    Switch db from sqlite to postgres

--- BrandensLibrary/settings.py
+++ BrandensLibrary/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'BrandensLibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'library',
+        'USER': 'admin',
+        'PASSWORD': 'iybmb1cbyl!p',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- authors/models.py
+++ authors/models.py
@@ -6,7 +6,6 @@ class Author(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    bio = models.TextField(blank=True, null=True)
     headshot = models.ImageField(null=True, blank=True, default="default.jpg")
 
     def __str__(self):
